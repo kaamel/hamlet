e75578ff0c38b3085bf8da6f05237422f5e15776
manoj <manoj@flipagram.com>
2017-10-15 11:12:28 -0700

    Using default client id

--- app/src/main/java/com/genesis/hamlet/ui/login/LoginActivity.java
+++ app/src/main/java/com/genesis/hamlet/ui/login/LoginActivity.java
@@ -91,7 +91,7 @@ public class LoginActivity extends AppCompatActivity implements
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
